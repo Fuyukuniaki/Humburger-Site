007ca902e02d3d0ef965dacf0b171a0e36d72160
Fuyukuniaki <info@a5-w.com>
2022-09-09 23:32:20 +0900

    [Modify] class名を規則に沿わせる

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function(){
 //-------------------------------------------------------------------------
     if(window.matchMedia("(max-width: 834px)").matches){
-        $('.js-body').prepend('<span class="p-button--clickable js-button--clickable">Menu</span>');
-        $('.js-button--clickable').each(function(){
+        $('.js-body').prepend('<span class="p-button__clickable js-button__clickable">Menu</span>');
+        $('.js-button__clickable').each(function(){
             $(this).on('click', function(){
                 if($(this).hasClass('clicked')){
                     $('body').removeClass('inactive');
