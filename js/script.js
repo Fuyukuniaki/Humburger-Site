d0cc8930a54c4d87782fd09ebf2d58aa2389d4e1
Fuyukuniaki <info@a5-w.com>
2022-09-11 22:46:48 +0900

    [Modify] menuまわり

--- js/script.js
+++ js/script.js
@@ -2,23 +2,32 @@ $(function(){
 //-------------------------------------------------------------------------
     if(window.matchMedia("(max-width: 834px)").matches){
         $('.js-body').prepend('<span class="p-button__clickable js-button__clickable">Menu</span>');
+        $('.js-body').prepend('<div class="js-shade p-body__Shade"></div>');
+
         $('.js-button__clickable').each(function(){
             $(this).on('click', function(){
                 if($(this).hasClass('clicked')){
-                    $('body').removeClass('inactive');
-                    $('nav').removeClass('active');
+                    $('.p-body__Shade').animate(
+                        { opacity: 0 }, 1000,
+                        function(){$(this).removeClass('inactive');}
+                    );
+                    $('nav').animate(
+                        { right: '-100%' }, 1000,
+                        function(){ $(this).removeClass('active');}
+                    );
                 } else {
-                    $('body').addClass('inactive');
-                    $('nav').addClass('active');
+                        $('.p-body__Shade').addClass('inactive').animate(
+                            { opacity: 1 }, 1000
+                        );
+                        $('nav').addClass('active').animate(
+                            { right: 0 }, 1000
+                        );
                 }
                 $(this).toggleClass('clicked');
             });
         });
     }
-
 });
-
-
 
 
 
