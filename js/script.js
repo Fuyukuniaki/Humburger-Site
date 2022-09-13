95afd7a831764fa7d181ae3282209eccbde18ffa
Fuyukuniaki <info@a5-w.com>
2022-09-13 22:12:11 +0900

    [Modify] Menuまわり

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-$(function(){
+
 //-------------------------------------------------------------------------
+function menu_slide(){
+    if($('.js-button__clickable').length > 0 ||
+       $('.js-button__clickable').hasClass('clicked') ||
+       $('nav').attr('right','0')){
+            $('.js-button__clickable').remove();
+            $('.js-shade').removeClass('inactive').remove();
+            $('.js-body').removeClass('inactive');
+            $('nav').css('right','-100%').removeClass('active');
+            $('.js-shade').css('opacity','0');
+            $('.js-button__clickable').removeClass('clicked');
+    }
     if(window.matchMedia("(max-width: 834px)").matches){
         $('.js-body').prepend('<span class="p-button__clickable js-button__clickable">Menu</span>');
         $('.js-body').prepend('<div class="js-shade p-body__Shade"></div>');
@@ -7,33 +18,32 @@ $(function(){
         $('.js-button__clickable').each(function(){
             $(this).on('click', function(){
                 if($(this).hasClass('clicked')){
-                    $('.p-body__Shade').animate(
-                        { opacity: 0 }, 1000,
+                    $('.js-body').removeClass('inactive');
+                    $('.js-shade').animate(
+                        { opacity: 0 }, 500,
                         function(){$(this).removeClass('inactive');}
                     );
                     $('nav').animate(
-                        { right: '-100%' }, 1000,
+                        { right: '-100%' }, 500,
                         function(){ $(this).removeClass('active');}
                     );
                 } else {
-                        $('.p-body__Shade').addClass('inactive').animate(
-                            { opacity: 1 }, 1000
-                        );
-                        $('nav').addClass('active').animate(
-                            { right: 0 }, 1000
-                        );
+                    $('.js-body').addClass('inactive');
+                    $('.js-shade').addClass('inactive').animate(
+                        { opacity: 1 }, 500
+                    );
+                    $('nav').addClass('active').animate(
+                        { right: 0 }, 500
+                    );
                 }
                 $(this).toggleClass('clicked');
             });
         });
     }
-});
+};
 
 
-
-
-
-
-$('window').on('resize', function(){
+$(window).on('load resize', function(){
+    menu_slide();
 //-------------------------------------------------------------------------
 });
